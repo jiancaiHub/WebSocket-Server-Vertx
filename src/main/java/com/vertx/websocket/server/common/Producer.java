6c5f776d0ca22f1c296ad3433211cc4cce51f796
jiancaiHub <634745237@qq.com>
2017-07-04 15:24:43 +0800

    实现消息主题消费端的过滤（消费路径添加过滤）

--- src/main/java/com/vertx/websocket/server/common/Producer.java
+++ src/main/java/com/vertx/websocket/server/common/Producer.java
@@ -7,8 +7,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 /**
  * Created by jiancai.wang on 2017/4/20.
@@ -23,16 +25,16 @@ public class Producer implements Serializable {
     // 标识符
     private String name;
     // 消息主题
-    private String topic;
+    private List<String> topicList;
     // 上线时间
     private Date timestamp;
     // 发送消息计数器
     public final AtomicLong receivedMsgCounter = new AtomicLong(0);
 
-    public Producer(Vertx vertx, ServerWebSocket webSocket, String topic) {
+    public Producer(Vertx vertx, ServerWebSocket webSocket, List<String> topicList) {
         this.vertx = vertx;
         this.webSocket = webSocket;
-        this.topic = topic;
+        this.topicList = topicList;
         this.name = "producer_" + UUID.randomUUID();
         this.timestamp = new Date();
     }
@@ -41,7 +43,7 @@ public class Producer implements Serializable {
     public String toString() {
         return "Producer{" +
                 "name='" + name + '\'' +
-                ", topic='" + topic + '\'' +
+                ", topic='" + topicList.stream().collect(Collectors.joining("[", "]", ", ")) + '\'' +
                 ", timestamp=" + timestamp +
                 '}';
     }
@@ -50,25 +52,22 @@ public class Producer implements Serializable {
 
         log.info("Producer online [ {} ]", this.getName());
         webSocket.closeHandler(close ->
-            log.info("Producer offline [ {} ]", this.getName())
+                log.info("Producer offline [ {} ]", this.getName())
         );
         return this;
     }
 
     public void access() {
-        // 消费数据
         webSocket.handler(buf -> {
             receivedMsgCounter.incrementAndGet();
             byte[] bytes = buf.copy().getBytes();
-            vertx.eventBus().publish(topic, bytes);
+            for (String topic : topicList) {
+                vertx.eventBus().publish(topic, bytes);
+            }
         });
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getTopic() {
-        return topic;
     }
 }
